644ee55379b012dd019a12e017685aa8cb5b3159
kushagra kamal <96110596+kushagra-01@users.noreply.github.com>
2022-06-26 00:56:02 +0530

    make it better

--- backend/src/models/product.model.js
+++ backend/src/models/product.model.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
-  {
+  {    name: { type: String,required:true },
     image: { type: String },
-    name: { type: String,required:true },
+
     brand: { type: String },
     category: { type: String },
     type: { type: String },
