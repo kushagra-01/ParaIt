452b8990d3336999c262ed31090c20ec817af0ab
kushagra kamal <96110596+kushagra-01@users.noreply.github.com>
2022-06-25 10:55:53 +0530

    authenticationDone

---
+++ backend/src/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const connect = require("./config/db");
+const { login, register } = require("./controllers/auth.controller");
+
+
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+
+
+app.post("/login", login);
+app.post("/register", register);
+
+//connecting ans starting server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, async () => {
+  try {
+    await connect();
+    console.log("listening on port 5000.....");
+  } catch (err) {
+    console.log(err);
+  }
+});
